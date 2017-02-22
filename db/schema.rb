3ae520b1c794076bea1d835906c246b2b7ba0e4e
hb90 <h90barron@gmail.com>
2017-02-22 22:01:23 +0000

    Add password to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222211436) do
+ActiveRecord::Schema.define(version: 20170222213155) do
 
   create_table "copiers", force: :cascade do |t|
     t.string   "name"
@@ -26,8 +26,9 @@ ActiveRecord::Schema.define(version: 20170222211436) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
